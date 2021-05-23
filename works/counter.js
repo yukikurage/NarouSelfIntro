28822b24bad9c6f122adedcb678d67b4fda4f12d
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-05-23 16:04:41 +0900

    Resetボタン追加

--- works/counter.js
+++ works/counter.js
@@ -1,6 +1,9 @@
 let count = 0
 const countUp = () => {
-  count++
-  const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+    count++
+    const countElement = document.querySelector('#count')
+    countElement.innerText = '回数: ' + count
+}
+const reset = () => {
+    count = 0
 }
